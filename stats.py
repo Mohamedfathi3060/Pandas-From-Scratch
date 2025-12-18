beea668825b4e5ab29f7d8b6f756058a37357b72
Mohamed Fathi <Mohamedfathi11119@gmail.com>
2025-12-18 12:56:41 +0200

    Implement Stat Lib Methods

--- stats.py
+++ stats.py
@@ -1,3 +1,5 @@
+from functools import reduce
+import file_handler
 def get_col_max(col:list):
     """
     Compute the maximum value of a numerical column.
@@ -8,7 +10,7 @@ def get_col_max(col:list):
     Returns:
         The maximum value in the column (numeric type).
     """
-    pass
+    return reduce(lambda x, y: y if (x is None) or ((y is not None) and x < y) else x, col)
 
 def get_col_min(col:list):
     """
@@ -20,7 +22,7 @@ def get_col_min(col:list):
     Returns:
         The minimum value in the column (numeric type).
     """
-    pass
+    return reduce(lambda x, y: y if (x is None) or ((y is not None) and x > y) else x, col)
 
 def get_col_mean(col:list):
     """
@@ -32,7 +34,17 @@ def get_col_mean(col:list):
     Returns:
         The mean value of the column (float).
     """
-    pass
+    try:
+        count = 0
+        sum = 0.0
+        for el in col:
+            if el is not None:
+                sum += el
+                count += 1
+        return  (sum / count).__round__(5)
+    except:
+        print("Non Numerical Column")
+        return 0
 
 def get_col_median(col:list):
     """
@@ -44,8 +56,12 @@ def get_col_median(col:list):
     Returns:
         The median value of the column (numeric type).
     """
-    pass
-    
+    col = custom_sort(col)
+    if len(col) % 2 == 1:
+        return col[len(col) // 2]
+    else:
+        return (col[(len(col) // 2)] + col[(len(col) // 2) -1] ) / 2
+
 def get_col_mode(col:list):
     """
     Compute the mode (most frequent value) of a column.
@@ -57,7 +73,20 @@ def get_col_mode(col:list):
         The mode value of the column. If multiple values have the same
         frequency, the first encountered is returned.
     """
-    pass
+    dict = {}
+    for el in col:
+        if el is not None:
+            try:
+                dict[el] += 1
+            except:
+                dict[el] = 1
+    max_col = ""
+    max_count = 0
+    for key, val in dict.items():
+        if val > max_count:
+            max_col = key
+            max_count = val
+    return max_col
         
 def get_stat(data:dict, dtypes:dict, function):
     """
@@ -72,9 +101,40 @@ def get_stat(data:dict, dtypes:dict, function):
         dict: A dictionary where keys are column names and values are the result
         of applying the function to that column. Only numerical columns are processed.
     """
-    pass
+    stringToFunction = {
+        'get_col_max' : get_col_max,
+        'get_col_min': get_col_min,
+        'get_col_mean':get_col_mean,
+        'get_col_median':get_col_median,
+        'get_col_mode':get_col_mode
+    }
+    if function not in [get_col_max,get_col_min, get_col_mean, get_col_median, get_col_mode]:
+        function = stringToFunction[function]
+    res = {}
+    for key, val in data.items():
+        if dtypes[key] == "string" and function != get_col_mode:
+            continue
+        res[key] = function(val)
+    return res
 
+def custom_sort(arr:list):
+    change = True
+    while change:
+        change = False
+        for i  in range(len(arr)-1):
+            if arr[i] > arr[i+1]:
+                tmp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = tmp
+                change = True
+    return arr
 
+# print(get_stat({'A':[-10,None,10],
+#           'B':[10,None, -10],
+#           'C':['A','B','A','E','E','E','A','E','E']
+#           }, {'A':'int','B':'float','C':'string'}, 'get_col_max'))
 
-
-
+# dtypes = file_handler.read_dtype()
+# data = file_handler.read_csv_file(dtypes=dtypes)
+#
+# print(get_stat(data, dtypes, 'get_col_mean'))
